673839aa277b0c56741d779e792c5b9a979e70a8
Juliano Cesar Magoga <julianoc.m@hotmail.com>
2023-11-15 17:47:47 -0300

    added more currency to the function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,41 @@
-cotacao = {
-  yen: 150.3,
-  real: 4.87,
-  won: 1305.14,
-  southAfrican: 18.21,
-}
-
 function currencyConverter() {
   const valueDolar = document.querySelector("#valueDolar")
   const spanResp = document.querySelector("#resp")
-  const cotacaoEmDolar = 4.87
 
-  const valueReal = valueDolar.value * cotacaoEmDolar
+  const cotacao = {
+    yen: 150.3,
+    real: 4.87,
+    won: 1305.14,
+    southAfrican: 18.21,
+    bitcoin: 0.000027,
+  }
 
+  const valueReal = valueDolar.value * cotacao.real
+  const valueYen = valueDolar.value * cotacao.yen
+  const valueWon = valueDolar.value * cotacao.won
+  const valueSouthAfrican = valueDolar.value * cotacao.southAfrican
+  const valueBTC = valueDolar.value * cotacao.bitcoin
   spanResp.classList.add("resp")
-  const msg = `A cotação é R$ ${cotacaoEmDolar.toFixed(
+  const msg = `A cotação do real é de R$ ${cotacao.real.toFixed(
     2
-  )}, e a conversão é R$ ${valueReal.toFixed(2)}`
+  )}, e a conversão é R$ ${valueReal.toFixed(
+    2
+  )},</br>A cotação do Yen é de ¥ ${cotacao.yen.toFixed(
+    2
+  )}, e a conversão é ¥ ${valueYen.toFixed(
+    2
+  )},</br>A cotação do Won é de ₩ ${cotacao.won.toFixed(
+    2
+  )}, e a conversão é ₩ ${valueWon.toFixed(
+    2
+  )},</br>A cotação do South African(ZAR) é de R ${cotacao.southAfrican.toFixed(
+    2
+  )}, e a conversão é R ${valueSouthAfrican.toFixed(
+    2
+  )},</br>A cotação do Bitcoin(₿) é de ₿ ${
+    cotacao.bitcoin
+  }, e a conversão é ₿ ${valueBTC}
+  `
 
-  return (spanResp.innerText = msg)
+  return (spanResp.innerHTML = msg)
 }
